7b9635ed51532c633cd0c721b933f3c27ac1367c
Thalia Archibald <thalia@archibald.dev>
2025-05-24 10:12:44 -0700

    Populate tar from automatic segments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-use std::fs::{self, File};
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+};
 
-use unix_1972_bits::{block::segment_blocks, s1::Segments, tap::Entry};
+use unix_1972_bits::{
+    block::{SegmentKind, segment_blocks},
+    interval::IntervalSet,
+    s1::Segment,
+    tap::Entry,
+};
 
 fn main() {
     let s2 = fs::read("s2-bits").unwrap();
@@ -21,52 +29,57 @@ fn main() {
         .comment(Some(b'#'))
         .from_path("s1-segments.csv")
         .unwrap();
-    let mut segments = Segments::new(&s1);
+    let mut intervals = IntervalSet::new(0..s1.len());
+    let mut by_offset = HashMap::new();
     for res in csv.deserialize() {
-        segments.insert(res.unwrap()).unwrap();
+        let segment: Segment = res.unwrap();
+        intervals.insert(segment.range()).unwrap();
+        if by_offset.insert(segment.offset, segment).is_some() {
+            panic!("duplicate offset");
+        }
     }
 
+    let segments = segment_blocks(&s1, 512);
     let mut tar = tar::Builder::new(File::create("s1-segments.tar").unwrap());
-    for segment in segments.segments {
-        let data = &s1[segment.range()];
+    for segment in segments {
         let mut h = tar::Header::new_old();
-        let path = segment.path.strip_prefix("/").unwrap_or(&segment.path);
-        h.set_path(format!("segments/{path}")).unwrap();
-        h.set_mode(0o644);
-        h.set_size(data.len() as _);
-        h.set_cksum();
-        tar.append(&h, data).unwrap();
-    }
-    let mut chunks = Vec::new();
-    for (i, block) in s1.chunks(512).enumerate() {
-        let range = i * 512..i * 512 + block.len();
-        chunks.clear();
-        segments.intervals.get_disjoint(range.clone(), &mut chunks);
-        for chunk in &chunks {
-            let data = &s1[chunk.clone()];
-            let typ = if data.iter().all(|&b| matches!(b, 0x07..=0x0f | b' '..=b'~')) {
+        let path = if let Some(named) = by_offset.remove(&segment.offset) {
+            if named.len != segment.data.len() {
+                eprintln!(
+                    "segment {} at offset {} has length {}; expected {}",
+                    named.path,
+                    segment.offset,
+                    segment.data.len(),
+                    named.len,
+                );
+            }
+            if named.path == "copy" {
+                continue;
+            }
+            let path = named.path.strip_prefix("/").unwrap_or(&named.path);
+            format!("files/{path}")
+        } else {
+            let ext = if segment
+                .data
+                .iter()
+                .all(|&b| matches!(b, 0x07..=0x0f | b' '..=b'~'))
+            {
                 "txt"
             } else {
                 "bin"
             };
-            let offset = chunk.start - range.start;
-            let path = if offset == 0 {
-                format!("blocks/block{i}.{typ}")
-            } else {
-                format!("blocks/block{i}.{offset}.{typ}")
+            let kind = match segment.kind {
+                SegmentKind::Original => "",
+                SegmentKind::Copy => ".copy",
+                SegmentKind::AllNul => ".nul",
+                SegmentKind::AllFF => ".ff",
             };
-            let mut h = tar::Header::new_old();
-            h.set_path(path).unwrap();
-            h.set_mode(0o644);
-            h.set_size(chunk.len() as _);
-            h.set_cksum();
-            tar.append(&h, data).unwrap();
-        }
-    }
-    println!("{:?}", segments.intervals);
-
-    let segments = segment_blocks(&s1, 512);
-    for segment in segments {
-        println!("{segment:?}");
+            format!("segments/{}{kind}.{ext}", segment.offset)
+        };
+        h.set_path(path).unwrap();
+        h.set_mode(0o644);
+        h.set_size(segment.data.len() as _);
+        h.set_cksum();
+        tar.append(&h, segment.data).unwrap();
     }
 }
