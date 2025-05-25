9abfee43d8b16b58729a4328abcddf1352e45974
Thalia Archibald <thalia@archibald.dev>
2025-05-25 03:41:14 -0700

    Track file headers in segmenter

--- src/block.rs
+++ src/block.rs
@@ -1,6 +1,17 @@
-use std::fmt;
+use std::{collections::HashMap, fmt};
 
-use crate::debug::Bytes;
+use anyhow::Result;
+
+use crate::{debug::Bytes, interval::IntervalSet, s1::FileSegment};
+
+pub struct Segmenter<'a> {
+    tape: &'a [u8],
+    block_size: usize,
+    prev_block: &'a [u8],
+    pub segments: Vec<Segment<'a>>,
+    pub headers: HashMap<usize, FileSegment>,
+    header_intervals: IntervalSet,
+}
 
 #[derive(Clone, PartialEq, Eq)]
 pub struct Segment<'a> {
@@ -17,83 +28,103 @@ pub enum SegmentKind {
     AllFF,
 }
 
-pub fn segment_blocks<'a>(data: &'a [u8], block_size: usize) -> Vec<Segment<'a>> {
-    let mut segments = Vec::new();
-
-    let mut prev = &[][..];
-    let data_ptr = data.as_ptr() as usize;
-    let mut start_offset = 0;
-
-    for block in data.chunks(block_size) {
-        let ptrs = block.as_ptr_range();
-        let offsets = ptrs.start as usize - data_ptr..ptrs.end as usize - data_ptr;
-
-        // Check for all NULL or all 0xFF.
-        let special = if block.iter().all(|&b| b == 0) {
-            Some(SegmentKind::AllNul)
-        } else if block.iter().all(|&b| b == 0xFF) {
-            Some(SegmentKind::AllFF)
-        } else {
-            None
-        };
-        if let Some(kind) = special {
-            if start_offset != offsets.start {
-                segments.push(Segment {
-                    data: &data[start_offset..offsets.start],
-                    offset: start_offset,
-                    kind: SegmentKind::Original,
-                });
-            }
-            segments.push(Segment {
-                data: block,
-                offset: offsets.start,
-                kind,
-            });
-            start_offset = offsets.end;
-            prev = block;
-            continue;
+impl<'a> Segmenter<'a> {
+    pub fn new(tape: &'a [u8], block_size: usize) -> Self {
+        Segmenter {
+            tape,
+            block_size,
+            prev_block: &[],
+            segments: Vec::new(),
+            headers: HashMap::new(),
+            header_intervals: IntervalSet::new(0..tape.len()),
         }
+    }
 
-        // Check whether this block and the previous have a common suffix.
-        if !prev.is_empty() {
-            let mut eq_index = 0;
-            for i in (0..block.len()).rev() {
-                if block[i] != prev[i] {
-                    eq_index = i + 1;
-                    break;
-                }
-            }
-            // Apparent copies of length 1 or 2 are usually false positives.
-            if eq_index + 2 < block.len() {
-                let split = offsets.start + eq_index;
-                if start_offset != split {
-                    segments.push(Segment {
-                        data: &data[start_offset..split],
-                        offset: start_offset,
+    pub fn add_header(&mut self, header: FileSegment) -> Result<()> {
+        self.header_intervals.insert(header.range())?;
+        assert!(self.headers.insert(header.offset, header).is_none());
+        Ok(())
+    }
+
+    /// Partitions a tape into segments which are likely to be files.
+    ///
+    /// It exploits the behavior of the dumping program that was used, which
+    /// evidently read files using a 512-byte buffer and directly dumped the
+    /// buffer. When the buffer is not completely filled, as happens for the
+    /// last block of a file, its tail is left unchanged, so by comparing blocks
+    /// for common tails, file boundaries can be quite accurately identified.
+    pub fn segment_blocks(&mut self) {
+        let mut segment_start = 0;
+
+        for block_start in (0..self.tape.len()).step_by(self.block_size) {
+            let block_end = (block_start + self.block_size).min(self.tape.len());
+            let block = &self.tape[block_start..block_end];
+
+            // Check for all NULL or all 0xFF.
+            let special = if block.iter().all(|&b| b == 0) {
+                Some(SegmentKind::AllNul)
+            } else if block.iter().all(|&b| b == 0xFF) {
+                Some(SegmentKind::AllFF)
+            } else {
+                None
+            };
+            if let Some(kind) = special {
+                if segment_start != block_start {
+                    self.segments.push(Segment {
+                        data: &self.tape[segment_start..block_start],
+                        offset: segment_start,
                         kind: SegmentKind::Original,
                     });
                 }
-                segments.push(Segment {
-                    data: &data[split..offsets.end],
-                    offset: split,
-                    kind: SegmentKind::Copy,
+                self.segments.push(Segment {
+                    data: block,
+                    offset: block_start,
+                    kind,
                 });
-                start_offset = offsets.end;
+                segment_start = block_end;
+                self.prev_block = block;
+                continue;
             }
+
+            // Check whether this block and the previous have a common suffix.
+            if !self.prev_block.is_empty() {
+                let mut eq_index = 0;
+                for i in (0..block.len()).rev() {
+                    if block[i] != self.prev_block[i] {
+                        eq_index = i + 1;
+                        break;
+                    }
+                }
+                // Apparent copies of length 1 or 2 are usually false positives.
+                if eq_index + 2 < block.len() {
+                    let split = block_start + eq_index;
+                    if segment_start != split {
+                        self.segments.push(Segment {
+                            data: &self.tape[segment_start..split],
+                            offset: segment_start,
+                            kind: SegmentKind::Original,
+                        });
+                    }
+                    self.segments.push(Segment {
+                        data: &self.tape[split..block_end],
+                        offset: split,
+                        kind: SegmentKind::Copy,
+                    });
+                    segment_start = block_end;
+                }
+            }
+
+            self.prev_block = block;
         }
 
-        prev = block;
+        if segment_start != self.tape.len() {
+            self.segments.push(Segment {
+                data: &self.tape[segment_start..],
+                offset: segment_start,
+                kind: SegmentKind::Original,
+            });
+        }
     }
-
-    if start_offset != data.len() {
-        segments.push(Segment {
-            data: &data[start_offset..],
-            offset: start_offset,
-            kind: SegmentKind::Original,
-        });
-    }
-
-    segments
 }
 
 impl fmt::Debug for Segment<'_> {
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 use std::{
-    collections::HashMap,
     ffi::OsStr,
     fs::{self, File},
     os::unix::ffi::OsStrExt,
 };
 
 use unix_1972_bits::{
-    block::{SegmentKind, segment_blocks},
+    block::{SegmentKind, Segmenter},
     debug::Bytes,
-    interval::IntervalSet,
-    s1::FileSegment,
     tap::Header,
 };
 
@@ -17,10 +14,8 @@ fn main() {
     let s2 = fs::read("s2-bits").unwrap();
 
     let mut tar = tar::Builder::new(File::create("s2-files.tar").unwrap());
-    for (i, chunk) in s2.chunks_exact(64).enumerate() {
+    for chunk in s2.chunks_exact(64) {
         if let Some(file) = Header::parse(chunk.try_into().unwrap()) {
-            let start = i * 64;
-            println!("{start}: {file:?}");
             let data = &s2[file.range()];
             tar.append(&file.to_tar_header(), data).unwrap();
         }
@@ -28,25 +23,21 @@ fn main() {
 
     let s1 = fs::read("s1-bits").unwrap();
 
+    let mut segmenter = Segmenter::new(&s1, 512);
+
     let mut csv = csv::ReaderBuilder::new()
         .comment(Some(b'#'))
         .from_path("s1-segments.csv")
         .unwrap();
-    let mut intervals = IntervalSet::new(0..s1.len());
-    let mut by_offset = HashMap::new();
-    for res in csv.deserialize() {
-        let file: FileSegment = res.unwrap();
-        intervals.insert(file.range()).unwrap();
-        if by_offset.insert(file.offset, file).is_some() {
-            panic!("duplicate offset");
-        }
+    for header in csv.deserialize() {
+        segmenter.add_header(header.unwrap()).unwrap();
     }
 
-    let segments = segment_blocks(&s1, 512);
+    segmenter.segment_blocks();
     let mut tar = tar::Builder::new(File::create("s1-segments.tar").unwrap());
-    for segment in segments {
+    for segment in &segmenter.segments {
         let mut h = tar::Header::new_old();
-        if let Some(named) = by_offset.remove(&segment.offset) {
+        if let Some(named) = segmenter.headers.get(&segment.offset) {
             if named.len != segment.data.len() {
                 eprintln!(
                     "segment {:?} at offset {} has length {}; expected {}",
