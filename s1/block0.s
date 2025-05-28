b2b5d7e2f7661165e228f3c068a5e5f5e0124874
Thalia Archibald <thalia@archibald.dev>
2025-05-28 06:09:17 -0700

    Name disk control registers

--- s1/block0.s
+++ s1/block0.s
@@ -1,19 +1,25 @@
 / Block 0 of s1-bits
 
-/ See TC11 DECtape System Manual (DEC-11-HTCB-D) [https://vt100.net/manx/details/1,3529]
-/ - Figure 4-2 for TCCM bits
-
-/ Tape control registers
-tcst = 177340	/ Control and status register
-tccm = 177342	/ Command register
-tcwc = 177344	/ Word count register
-tcba = 177346	/ Bus address register
-tcdt = 177350	/ Data register
+/ Tape control registers [https://gunkies.org/wiki/TC11_DECtape_controller]
+tcst =	177340				/ #define TCST	0177340	/* Control and status register */
+tccm =	177342				/ #define TCCM	0177342	/* Command register */
+tcwc =	177344				/ #define TCWC	0177344	/* Word count register */
+tcba =	177346				/ #define TCBA	0177346	/* Bus address register */
+tcdt =	177350				/ #define TCDT	0177350	/* Data register */
+/ Disk control registers [https://gunkies.org/wiki/RF11_disk_controller]
+dcs =	177460				/ #define DCS	0177460	/* Disk control status register */
+wc =	177462				/ #define WC	0177462	/* Word count register */
+cma =	177464				/ #define CMA	0177464	/* Current memory address */
+dar =	177466				/ #define DAR	0177466	/* Disk address register */
+dae =	177470				/ #define DAE	0177470	/* Disk address extension error register */
+dbr =	177472				/ #define DBR	0177472	/* Data buffer register */
+ma =	177474				/ #define MA	0177474	/* Maintenance register */
+ads =	177476				/ #define ADS	0177476	/* Address of disk segment register */
 
 	mov	$20000,sp
-	jsr	r5,init			/ init(data);
+	jsr	r5,init			/ init(values);
 
-000001; 020000; 160000; 000005		/ int data[] = { ... };
+000001; 020000; 160000; 000005		/ int values[] = { ... };
 004567; 000204; 000003; 140000
 020000; 160000; 000003; 004567
 000050; 000041; 020000; 160000
@@ -23,81 +29,81 @@ tcdt = 177350	/ Data register
 054000; 176000; 000005; 000137
 054000
 
-					/ /* Command values */
+					/ /* TCCM command values */
 					/ #define DO	1	/* Give a new function */
 					/ #define RNUM	2	/* Function: read block number */
 					/ #define TAPE0	00000	/* Select tape unit 0 */
 					/ #define FWD	00000	/* Forward direction */
 					/ #define REV	04000	/* Reverse direction */
 
-					/ /* Wait until bit 7 (ready) of TCCM is set, indicating
+					/ /* Wait until TCCM bit 7 (ready) is set, indicating
 					/  * that the current command has completed execution. */
-					/ #define wait() while (*(char *)tccm >= 0)
+					/ #define wait() while (*(char *)cmd >= 0)
 
-					/ /* Test whether bit 15 (error) of TCCM is set. */
+					/ /* Test whether TCCM bit 15 (error) is set. */
 					/ #define error()
 
-init:					/ init(data)
-					/ int *data; /* r5 */
+init:					/ init(values)
+					/ int *values; /* r5 */
 					/ {
-					/	register int *tcdt; /* r0 */
-					/	register int *tccm; /* r1 */
-					/	register x2; /* r2 */
+					/	register int *data; /* r0 */
+					/	register int *cmd; /* r1 */
+					/	register int *dp; /* r2 */
 seekfwd:				/ seekfwd:
-	mov	$tcdt,r0		/	tcdt = 0177350; /* Tape control data register */
-	mov	$tccm,r1		/	tccm = 0177342; /* Tape control command register */
-	mov	$3,(r1)			/	*tccm = DO | RNUM | TAPE0 | FWD;
+	mov	$tcdt,r0		/	data = TCDT;
+	mov	$tccm,r1		/	cmd = TCCM;
+	mov	$3,(r1)			/	*cmd = DO | RNUM | TAPE0 | FWD;
 1:
 	tstb	(r1)			/	wait();
 	bge	1b
 	tst	(r1)	/ error?	/	if (error())
 	blt	seekrev			/		goto seekrev;
-	cmp	(r5),(r0)		/	if (*data == *tcdt)
+	cmp	(r5),(r0)		/	if (*values == *data)
 	beq	found			/		goto found;
-	bgt	seekfwd			/	if (*data > *tcdt)
+	bgt	seekfwd			/	if (*values > *data)
 					/		goto seekfwd;
 seekrev:				/ seekrev:
-	mov	$4003,(r1)		/	*tccm = DO | RNUM | TAPE0 | REV;
+	mov	$4003,(r1)		/	*cmd = DO | RNUM | TAPE0 | REV;
 1:
 	tstb	(r1)			/	wait();
 	bge	1b
 	tst	(r1)			/	if (error())
 	blt	seekfwd			/		goto seekfwd;
-	mov	(r0),r2			/	x2 = *tcdt + 5;
+	mov	(r0),r2			/	dp = *data + 5;
 	add	$5,r2
-	cmp	(r5),r2			/	if (*data > x2)
+	cmp	(r5),r2			/	if (*values > dp)
 	bgt	seekfwd			/		goto seekfwd;
 	br	seekrev			/	goto seekrev;
 found:					/ found:
-	tst	(r5)+			/	data++;
-	mov	(r5)+,-(r0)		/	*--tcdt = *data++;
-	mov	(r5)+,-(r0)		/	*--tcdt = *data++;
-	mov	(r5)+,-(r0)		/	*--tcdt = *data++;
+	tst	(r5)+			/	values++;
+	mov	(r5)+,-(r0)		/	*--data = *values++;
+	mov	(r5)+,-(r0)		/	*--data = *values++;
+	mov	(r5)+,-(r0)		/	*--data = *values++;
 1:
 	tstb	(r0)			/	wait();
 	bge	1b
 	tst	(r0)	/ error?	/	if (error()) {
 	bge	2f
-	sub	$10,r5			/		data =- 4;
+	sub	$10,r5			/		values =- 4;
 	br	seekfwd			/		goto seekfwd;
 2:					/	}
-	mov	$1,(r0)			/	*tcdt = 1;
+	mov	$1,(r0)			/	*data = 1;
 	rts	r5			/	return;
 
-todo:					/ todo:
-	mov	$177472,r0		/	tcdt = 0177472; /* TODO */
-	mov	(r5)+,-(r0)		/	*--tcdt = *data++;
-	mov	(r5)+,-(r0)		/	*--tcdt = *data++;
-	mov	(r5)+,-(r0)		/	*--tcdt = *data++;
-	mov	(r5)+,-(r0)		/	*--tcdt = *data++;
-	mov	(r5)+,-(r0)		/	*--tcdt = *data++;
+disk:					/ disk:
+	mov	$dbr,r0			/	data = DBR;
+	mov	(r5)+,-(r0)		/	*--data = *values++;
+	mov	(r5)+,-(r0)		/	*--data = *values++;
+	mov	(r5)+,-(r0)		/	*--data = *values++;
+	mov	(r5)+,-(r0)		/	*--data = *values++;
+	mov	(r5)+,-(r0)		/	*--data = *values++;
 1:
 	tstb	(r0)			/	wait();
 	bge	1b
 	tst	(r0)			/	if (error()) {
 	bge	2f
-	sub	$12,r5			/		data =- 5;
-	br	todo			/		goto todo;
+	sub	$12,r5			/		values =- 5;
+	br	disk			/		goto disk;
 2:					/	}
 	rts	r5
 					/ }
