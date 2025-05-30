b5b0985636defdb8a5301cb0acf9082a3835ddcb
Thalia Archibald <thalia@archibald.dev>
2025-05-30 07:00:18 -0700

    Fix disk ops and describe buffer

--- s1/block0.s
+++ s1/block0.s
@@ -5,28 +5,29 @@
 / controller.
 /
 / References:
-/   *	TC11 DECtape system manual, Chapter 4: Programming Information,
+/   *	TC11 DECtape system manual, Chapter 4 Programming Information and Table 3-3,
 /	https://bitsavers.org/pdf/dec/dectape/tc11/TC11_Mantenance_Manual.pdf
-/   *	RF11/RS11 DECdisk system manual, Chapter 3: Programming,
+/   *	RF11/RS11 DECdisk system manual, Chapter 3 Programming and Section 1.3.3 Logic Format,
 /	https://bitsavers.org/pdf/dec/unibus/DEC-11-HRFB-D__RF11_RS11_DECdisk_System_Manual__Aug71.pdf
 /   *	https://gunkies.org/wiki/TC11_DECtape_controller
 /   *	https://gunkies.org/wiki/RF11_disk_controller
 
-/ Tape control registers
-tcst =	177340				/ #define TCST	((int *)0177340)	/* Control and status register */
+					/ /* Tape control registers */
+					/ #define TCST	((int *)0177340)	/* Control and status register */
 tccm =	177342				/ #define TCCM	((int *)0177342)	/* Command register */
-tcwc =	177344				/ #define TCWC	((int *)0177344)	/* Word count register */
-tcba =	177346				/ #define TCBA	((int *)0177346)	/* Bus address register */
+					/ #define TCWC	((int *)0177344)	/* Word count register */
+					/ #define TCBA	((int *)0177346)	/* Bus address register */
 tcdt =	177350				/ #define TCDT	((int *)0177350)	/* Data register */
-/ Disk control registers
-dcs =	177460				/ #define DCS	((int *)0177460)	/* Disk control status register */
-wc =	177462				/ #define WC	((int *)0177462)	/* Word count register */
-cma =	177464				/ #define CMA	((int *)0177464)	/* Current memory address */
-dar =	177466				/ #define DAR	((int *)0177466)	/* Disk address register */
-dae =	177470				/ #define DAE	((int *)0177470)	/* Disk address extension error register */
+
+					/ /* Disk control registers */
+					/ #define DCS	((int *)0177460)	/* Disk control status register */
+					/ #define WC	((int *)0177462)	/* Word count register */
+					/ #define CMA	((int *)0177464)	/* Current memory address */
+					/ #define DAR	((int *)0177466)	/* Disk address register */
+					/ #define DAE	((int *)0177470)	/* Disk address extension error register */
 dbr =	177472				/ #define DBR	((int *)0177472)	/* Data buffer register */
-ma =	177474				/ #define MA	((int *)0177474)	/* Maintenance register */
-ads =	177476				/ #define ADS	((int *)0177476)	/* Address of disk segment register */
+					/ #define MA	((int *)0177474)	/* Maintenance register */
+					/ #define ADS	((int *)0177476)	/* Address of disk segment register */
 
 					/ /* Common TCCM and DCS bits */
 					/ #define GO		1	/* Start executing a new function */
@@ -48,8 +49,8 @@ ads =	177476				/ #define ADS	((int *)0177476)	/* Address of disk segment regist
 
 					/ /* DCS bits */
 					/ #define D_NOP		(0<<1)	/* Function: nop */
-					/ #define D_READ	(1<<1)	/* Function: read */
-					/ #define D_WRITE	(2<<1)	/* Function: write */
+					/ #define D_WRITE	(1<<1)	/* Function: write */
+					/ #define D_READ	(2<<1)	/* Function: read */
 					/ #define D_WRTCK	(3<<1)	/* Function: write check */
 
 					/ /* DAE bits */
@@ -67,75 +68,88 @@ ads =	177476				/ #define ADS	((int *)0177476)	/* Address of disk segment regist
 					/ /* Test whether the DCS error bit is set. */
 					/ #define derror() (*DCS < 0)
 
-	mov	$20000,sp		/ #define MEMP	((int *)020000)	/* Base address to copy tape data to */
+	mov	$20000,sp		/ int buf[8192+7168];	/* Buffer at address 020000 */
 
 					/ main()
 					/ {
-					/	/* Read 8192 words from tape 0 to address MEMP */
+					/	/* Read 8192 words from tape 0 block 1 to buf.
+					/	 * Block 1 is at word 1*256. */
 	jsr	r5,tapecmd		/	tapecmd(
-	1				/		1,
-	20000	/ TCBA			/		MEMP,		/* TCBA: memory address */
+	1	/ Block number		/		1,		/* Block number 1 */
+	20000	/ TCBA			/		buf,		/* TCBA: memory address */
 	-20000	/ TCWC			/		-8192,		/* TCWC: transfer 8192 words */
-	5	/ TCCM			/		GO|T_RALL|TAPE(0)|T_FWD,	/* TCCM */
+	5	/ TCCM			/		GO|T_RALL|TAPE(0)|T_FWD,	/* TCCM: command */
 					/	);
-					/	/* Read 8192 words from disk 0 track 120 to address MEMP */
+					/	/* Write 8192 words from buf to disk 0 track 120.
+					/	 * Track 120 is at word 120*2048. */
 	jsr	r5,diskcmd		/	diskcmd(
 	3	/ DAE			/		03|DISK(0),	/* DAE: track address (bits 5-6) and disk */
 	140000	/ DAR			/		030<<11,	/* DAR: track address (bits 0-4) */
-	20000	/ CMA			/		MEMP,		/* CMA: memory address */
+	20000	/ CMA			/		buf,		/* CMA: memory address */
 	-20000	/ WC			/		-8192,		/* WC: transfer 8192 words */
-	3	/ DCS			/		GO|D_READ,	/* DCS */
+	3	/ DCS			/		GO|D_WRITE,	/* DCS: command */
 					/	);
+					/	/* Read 8192 words from tape 0 block 33 to buf.
+					/	 * Block 33 is at word 33*256, or 8192 words after block 1. */
 	jsr	r5,tapecmd		/	tapecmd(
-	41				/		33,
-	20000	/ TCBA			/		MEMP,		/* TCBA: memory address */
+	41	/ Block number		/		33,		/* Block number 33 */
+	20000	/ TCBA			/		buf,		/* TCBA: memory address */
 	-20000	/ TCWC			/		-8192,		/* TCWC: transfer 8192 words */
-	5	/ TCCM			/		GO|T_RDATA|TAPE(0)|T_FWD,	/* TCCM */
+	5	/ TCCM			/		GO|T_RALL|TAPE(0)|T_FWD,	/* TCCM: command */
 					/	);
-					/	/* Read 8192 words from disk 0 track 124 to address MEMP */
+					/	/* Write 8192 words from buf to disk 0 track 124.
+					/	 * Track 124 is at word 124*2048, or 8192 words after track 120. */
 	jsr	r5,diskcmd		/	diskcmd(
 	3	/ DAE			/		03|DISK(0),	/* DAE: track address (bits 5-6) and disk */
 	160000	/ DAR			/		034<<11,	/* DAR: track address (bits 0-4) */
-	20000	/ CMA			/		MEMP,		/* CMA: memory address */
+	20000	/ CMA			/		buf,		/* CMA: memory address */
 	-20000	/ WC			/		-8192,		/* WC: transfer 8192 words */
-	3	/ DCS			/		GO|D_READ,	/* DCS */
+	3	/ DCS			/		GO|D_WRITE,	/* DCS: command */
 					/	);
-					/	/* Write 8192 words from address 054000 to disk 0 track 120 */
+					/	/* Read 8192 words from disk 0 track 120 to address &buf[7168]. */
 	jsr	r5,diskcmd		/	diskcmd(
 	3	/ DAE			/		03|DISK(0),	/* DAE: track address (bits 5-6) and disk */
 	140000	/ DAR			/		030<<11,	/* DAR: track address (bits 0-4) */
-	54000	/ CMA			/		054000,		/* CMA: memory address */
+	54000	/ CMA			/		&buf[7168],	/* CMA: memory address */
 	-2000	/ WC			/		-1024,		/* WC: transfer 8192 words */
-	5	/ DCS			/		GO|D_WRITE,	/* DCS */
+	5	/ DCS			/		GO|D_READ,	/* DCS: command */
 					/	);
-	jmp	*$54000			/	goto *054000;
+	jmp	*$54000			/	(*(int (*)()) &buf[7168])();
 					/ };
 
-tapecmd:				/ tapecmd(dt, ba, wc, cm)
+					/ /* Seeks to the given block in tape 0, then writes
+					/  * ba, wc, and cm to TCBA, TCWC, and TCCM, respectively.
+tapecmd:				/ tapecmd(block, ba, wc, cm)
 					/ {
+					/	int b;
 seekfwd:				/ seekfwd:
 	mov	$tcdt,r0
 	mov	$tccm,r1
-	mov	$3,(r1)			/	*TCCM = GO | T_RNUM | TAPE(0) | T_FWD;
+					/	/* Seek forward in tape 0 to the next block
+					/	 * and read its block number into TCDT */
+	mov	$3,(r1)			/	*TCCM = GO|T_RNUM|TAPE(0)|T_FWD;
 1:
 	tstb	(r1)			/	tcwait();
 	bge	1b
 	tst	(r1)	/ error?	/	if (tcerror())
 	blt	seekrev			/		goto seekrev;
-	cmp	(r5),(r0)		/	if (dt == *TCDT)
+					/	b = *TCDT;
+	cmp	(r5),(r0)		/	if (b == block)
 	beq	found			/		goto found;
-	bgt	seekfwd			/	if (dt > *TCDT)
+	bgt	seekfwd			/	if (b < block)
 					/		goto seekfwd;
 seekrev:				/ seekrev:
-	mov	$4003,(r1)		/	*TCCM = GO | T_RNUM | TAPE(0) | T_REV;
+					/	/* Seek backward in tape 0 to the next block
+					/	 * and read its block number into TCDT */
+	mov	$4003,(r1)		/	*TCCM = GO|T_RNUM|TAPE(0)|T_REV;
 1:
 	tstb	(r1)			/	tcwait();
 	bge	1b
 	tst	(r1)			/	if (tcerror())
 	blt	seekfwd			/		goto seekfwd;
 	mov	(r0),r2
-	add	$5,r2
-	cmp	(r5),r2			/	if (dt > *TCDT + 5)
+	add	$5,r2			/	/* Let it over-seek, before switching back to forward */
+	cmp	(r5),r2			/	if (*TCDT + 5 < block)
 	bgt	seekfwd			/		goto seekfwd;
 	br	seekrev			/	goto seekrev;
 found:					/ found:
@@ -151,10 +165,12 @@ found:					/ found:
 	sub	$8.,r5
 	br	seekfwd			/		goto seekfwd;
 2:
-	mov	$1,(r0)			/	*TCCM = 1;
+					/	/* Stop tape motion for tape 0 */
+	mov	$1,(r0)			/	*TCCM = GO|T_SAT|TAPE(0);
 	rts	r5
 					/ }
 
+					/ /* Sends the given command to the disk controller. */
 diskcmd:				/ diskcmd(dae, dar, cma, wc, dcs)
 					/ {
 					/ retry:
