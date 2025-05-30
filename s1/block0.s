4391917372ff7064a33b2d2f787cfbd9876b61e7
Thalia Archibald <thalia@archibald.dev>
2025-05-30 05:28:35 -0700

    Describe disk commands

--- s1/block0.s
+++ s1/block0.s
@@ -1,12 +1,24 @@
 / Block 0 of s1-bits
 
-/ Tape control registers [https://gunkies.org/wiki/TC11_DECtape_controller]
+/ This reads data from the tape and reads from and writes to a disk by
+/ manipulating device registers for the RC11 DECtape controller and RF11 DECdisk
+/ controller.
+/
+/ References:
+/   *	TC11 DECtape system manual, Chapter 4: Programming Information,
+/	https://bitsavers.org/pdf/dec/dectape/tc11/TC11_Mantenance_Manual.pdf
+/   *	RF11/RS11 DECdisk system manual, Chapter 3: Programming,
+/	https://bitsavers.org/pdf/dec/unibus/DEC-11-HRFB-D__RF11_RS11_DECdisk_System_Manual__Aug71.pdf
+/   *	https://gunkies.org/wiki/TC11_DECtape_controller
+/   *	https://gunkies.org/wiki/RF11_disk_controller
+
+/ Tape control registers
 tcst =	177340				/ #define TCST	((int *)0177340)	/* Control and status register */
 tccm =	177342				/ #define TCCM	((int *)0177342)	/* Command register */
 tcwc =	177344				/ #define TCWC	((int *)0177344)	/* Word count register */
 tcba =	177346				/ #define TCBA	((int *)0177346)	/* Bus address register */
 tcdt =	177350				/ #define TCDT	((int *)0177350)	/* Data register */
-/ Disk control registers [https://gunkies.org/wiki/RF11_disk_controller]
+/ Disk control registers
 dcs =	177460				/ #define DCS	((int *)0177460)	/* Disk control status register */
 wc =	177462				/ #define WC	((int *)0177462)	/* Word count register */
 cma =	177464				/ #define CMA	((int *)0177464)	/* Current memory address */
@@ -16,88 +28,95 @@ dbr =	177472				/ #define DBR	((int *)0177472)	/* Data buffer register */
 ma =	177474				/ #define MA	((int *)0177474)	/* Maintenance register */
 ads =	177476				/ #define ADS	((int *)0177476)	/* Address of disk segment register */
 
-					/ /* TCCM bits */
-					/ #define DO		1	/* Give a new function */
-					/ #define SAT		(0<<1)	/* Function: stop all transports */
-					/ #define RNUM		(1<<1)	/* Function: read block number */
-					/ #define RDATA		(2<<1)	/* Function: read data */
-					/ #define RALL		(3<<1)	/* Function: read all */
-					/ #define SST		(4<<1)	/* Function: stop selected transport */
-					/ #define WRTM		(5<<1)	/* Function: write timing and mark trace */
-					/ #define WDATA		(6<<1)	/* Function: write data */
-					/ #define WALL		(7<<1)	/* Function: write all */
-					/ #define XBA16		(1<<4)	/* Extended bus address bit 16 */
-					/ #define XBA17		(1<<5)	/* Extended bus address bit 17 */
-					/ #define IE		(1<<6)	/* Interrupt enable */
+					/ /* Common TCCM and DCS bits */
+					/ #define GO		1	/* Start executing a new function */
 					/ #define READY		(1<<7)	/* Ready */
-					/ #define TAPE(n)	(n<<8)	/* Select tape unit n (0-7) */
-					/ #define FWD		(0<<11)	/* Forward direction */
-					/ #define REV		(1<<11)	/* Reverse direction */
-					/ #define DINHB		(1<<12)	/* Delay inhibit */
-					/ #define MAINT		(1<<13) /* Used for maintenance functions */
 					/ #define ERROR		(1<<15)	/* Error condition */
+
+					/ /* TCCM bits */
+					/ #define T_SAT		(0<<1)	/* Function: stop all transports */
+					/ #define T_RNUM	(1<<1)	/* Function: read block number */
+					/ #define T_RDATA	(2<<1)	/* Function: read data */
+					/ #define T_RALL	(3<<1)	/* Function: read all */
+					/ #define T_SST		(4<<1)	/* Function: stop selected transport */
+					/ #define T_WRTM	(5<<1)	/* Function: write timing and mark trace */
+					/ #define T_WDATA	(6<<1)	/* Function: write data */
+					/ #define T_WALL	(7<<1)	/* Function: write all */
+					/ #define TAPE(n)	(n<<8)	/* Select tape unit n (0-7) */
+					/ #define T_FWD		(0<<11)	/* Forward direction */
+					/ #define T_REV		(1<<11)	/* Reverse direction */
+
+					/ /* DCS bits */
+					/ #define D_NOP		(0<<1)	/* Function: nop */
+					/ #define D_READ	(1<<1)	/* Function: read */
+					/ #define D_WRITE	(2<<1)	/* Function: write */
+					/ #define D_WRTCK	(3<<1)	/* Function: write check */
+
+					/ /* DAE bits */
+					/ #define DISK(n)	(n<<2)	/* Select disk n (0-7) */
+
+					/ /* Wait until the TCCM ready bit is set, indicating
+					/  * that the current command has completed execution. */
+					/ #define tcwait() while (*(char *)TCCM >= 0)
+					/ /* Test whether the TCCM error bit is set. */
+					/ #define tcerror() (*TCCM < 0)
+
+					/ /* Wait until the DCS ready bit is set, indicating
+					/  * that the current command has completed execution. */
+					/ #define dwait() while (*(char *)TCCM >= 0)
+					/ /* Test whether the DCS error bit is set. */
+					/ #define derror() (*DCS < 0)
 
 	mov	$20000,sp		/ #define MEMP	((int *)020000)	/* Base address to copy tape data to */
 
 					/ main()
 					/ {
-					/	/* Word counts are negated. */
-					/
-					/	/* Read 8192 words from tape 0 to address MEMP. */
+					/	/* Read 8192 words from tape 0 to address MEMP */
 	jsr	r5,tapecmd		/	tapecmd(
 	1				/		1,
-	20000	/ TCBA			/		MEMP,		/* TCBA */
-	-20000	/ TCWC			/		-8192,		/* TCWC */
-	5	/ TCCM			/		DO | RALL | TAPE(0) | FWD,	/* TCCM */
+	20000	/ TCBA			/		MEMP,		/* TCBA: memory address */
+	-20000	/ TCWC			/		-8192,		/* TCWC: transfer 8192 words */
+	5	/ TCCM			/		GO|T_RALL|TAPE(0)|T_FWD,	/* TCCM */
 					/	);
+					/	/* Read 8192 words from disk 0 track 120 to address MEMP */
 	jsr	r5,diskcmd		/	diskcmd(
-	3	/ DAE			/		3,		/* DAE */
-	140000	/ DAR			/		0140000,	/* DAR */
-	20000	/ CMA			/		MEMP,		/* CMA */
-	-20000	/ WC			/		-8192,		/* WC */
-	3	/ DCS			/		3,		/* DCS */
+	3	/ DAE			/		03|DISK(0),	/* DAE: track address (bits 5-6) and disk */
+	140000	/ DAR			/		030<<11,	/* DAR: track address (bits 0-4) */
+	20000	/ CMA			/		MEMP,		/* CMA: memory address */
+	-20000	/ WC			/		-8192,		/* WC: transfer 8192 words */
+	3	/ DCS			/		GO|D_READ,	/* DCS */
 					/	);
 	jsr	r5,tapecmd		/	tapecmd(
 	41				/		33,
-	20000	/ TCBA			/		MEMP,		/* TCBA */
-	-20000	/ TCWC			/		-8192,		/* TCWC */
-	5	/ TCCM			/		DO | RDATA | TAPE(0) | FWD,	/* TCCM */
+	20000	/ TCBA			/		MEMP,		/* TCBA: memory address */
+	-20000	/ TCWC			/		-8192,		/* TCWC: transfer 8192 words */
+	5	/ TCCM			/		GO|T_RDATA|TAPE(0)|T_FWD,	/* TCCM */
 					/	);
+					/	/* Read 8192 words from disk 0 track 124 to address MEMP */
 	jsr	r5,diskcmd		/	diskcmd(
-	3	/ DAE			/		3,		/* DAE */
-	160000	/ DAR			/		0160000,	/* DAR */
-	20000	/ CMA			/		020000,		/* CMA */
-	-20000	/ WC			/		-8192,		/* WC */
-	3	/ DCS			/		3,		/* DCS */
+	3	/ DAE			/		03|DISK(0),	/* DAE: track address (bits 5-6) and disk */
+	160000	/ DAR			/		034<<11,	/* DAR: track address (bits 0-4) */
+	20000	/ CMA			/		MEMP,		/* CMA: memory address */
+	-20000	/ WC			/		-8192,		/* WC: transfer 8192 words */
+	3	/ DCS			/		GO|D_READ,	/* DCS */
 					/	);
+					/	/* Write 8192 words from address 054000 to disk 0 track 120 */
 	jsr	r5,diskcmd		/	diskcmd(
-	3	/ DAE			/		3,		/* DAE */
-	140000	/ DAR			/		0140000,	/* DAR */
-	54000	/ CMA			/		054000,		/* CMA */
-	-2000	/ WC			/		-1024,		/* WC */
-	5	/ DCS			/		5,		/* DCS */
+	3	/ DAE			/		03|DISK(0),	/* DAE: track address (bits 5-6) and disk */
+	140000	/ DAR			/		030<<11,	/* DAR: track address (bits 0-4) */
+	54000	/ CMA			/		054000,		/* CMA: memory address */
+	-2000	/ WC			/		-1024,		/* WC: transfer 8192 words */
+	5	/ DCS			/		GO|D_WRITE,	/* DCS */
 					/	);
 	jmp	*$54000			/	goto *054000;
 					/ };
-
-					/ /* Wait until TCCM bit 7 (ready) is set, indicating
-					/  * that the current command has completed execution. */
-					/ #define tcwait() while (*(char *)TCCM >= 0)
-					/ /* Test whether TCCM bit 15 (error) is set. */
-					/ #define tcerror() (*TCCM < 0)
-
-					/ /* Wait until DCS bit 7 (ready) is set, indicating
-					/  * that the current command has completed execution. */
-					/ #define dwait() while (*(char *)TCCM >= 0)
-					/ /* Test whether DCS bit 15 (error) is set. */
-					/ #define derror() (*DCS < 0)
 
 tapecmd:				/ tapecmd(dt, ba, wc, cm)
 					/ {
 seekfwd:				/ seekfwd:
 	mov	$tcdt,r0
 	mov	$tccm,r1
-	mov	$3,(r1)			/	*TCCM = DO | RNUM | TAPE(0) | FWD;
+	mov	$3,(r1)			/	*TCCM = GO | T_RNUM | TAPE(0) | T_FWD;
 1:
 	tstb	(r1)			/	tcwait();
 	bge	1b
@@ -108,7 +127,7 @@ seekfwd:				/ seekfwd:
 	bgt	seekfwd			/	if (dt > *TCDT)
 					/		goto seekfwd;
 seekrev:				/ seekrev:
-	mov	$4003,(r1)		/	*TCCM = DO | RNUM | TAPE(0) | REV;
+	mov	$4003,(r1)		/	*TCCM = GO | T_RNUM | TAPE(0) | T_REV;
 1:
 	tstb	(r1)			/	tcwait();
 	bge	1b
